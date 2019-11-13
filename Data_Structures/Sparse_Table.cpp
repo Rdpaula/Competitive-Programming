de5770d40a255a3ed22b343c8d22f2a92c2b8db0
Rdpaula <raphael@72008.hotmail.com>
2019-11-13 14:04:14 -0300

    Adding spt

---
+++ Data_Structures/Sparse_Table.cpp
@@ -0,0 +1,23 @@
+#define LG 20
+ll N,a[maxn], sptable[LG][maxn];
+///Thanks AmandioF
+void build() {
+    for(int i=0;i<N;i++) sptable[0][i] = a[i];
+
+    for(int i = 1; (1 << i) <= N; i++){
+        for(int j=0; j + (1 << i) <= N; j++){
+            sptable[i][j] = min(sptable[i-1][j], sptable[i-1][j + (1 << (i-1))]);
+        }
+    }
+}
+
+ll query(int a, int b) {
+    if(a > b) swap(a,b);
+    int lg = __builtin_clz(1) - __builtin_clz(b - a + 1);
+
+    return min(sptable[lg][a], sptable[lg][b -(1 << lg)+1]);
+}
+
+int main(){
+    return 0;
+}
