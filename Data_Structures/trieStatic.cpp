754da3caf44e4bc62c3b5d4a79816bf95797ed64
Rdpaula <raphael@72008.hotmail.com>
2020-02-06 23:36:56 -0300

    adding trieStatic

---
+++ Data_Structures/trieStatic.cpp
@@ -0,0 +1,24 @@
+int trie[maxx][26], finish[maxx];
+int nxt = 1;
+void Add (string s){
+    int dnode = 0;
+    for (int i = 0; s[i] != '\0'; i++) {
+        if(trie[node][s[i] - 'a'] == 0) {
+            node = trie[node][s[i] - 'a'] = nxt;
+            nxt++;
+        } else {
+            node = trie[node][s[i] - 'a'];
+        }
+    }
+    finish[nxt - 1] = 1;
+}
+
+int Find (string s) {
+    int idx = 0;
+    for (int i = 0; i < s.size(); i++)
+        if (trie[idx][s[i] - 'a'] == 0)
+            return 0;
+        else
+            idx = trie[idx][s[i] - 'a'];
+    return finish[idx];
+}
