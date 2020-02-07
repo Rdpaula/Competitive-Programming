5d17451d158202a99b0f0c1cd17b71afb926c46e
Rdpaula <raphael@72008.hotmail.com>
2020-02-07 01:29:02 -0300

    adding DynamicTrie

---
+++ Data_Structures/DynamicTrie.cpp
@@ -0,0 +1,27 @@
+struct node{
+	bool end;
+	struct node* word[26];
+} *head;
+
+void init(){
+	head = new node();
+	head->end = false;
+}
+
+void add(string palavra){
+	node* current = head;
+	for(int i = 0; i < palavra.size(); i++){
+		int letra = palavra[i]-'a';
+
+		if(current->word[letra] == NULL) current->word[letra] = new node();
+
+		current = current->word[letra];
+	}
+	current->end = true;
+}
+int main(){
+	string palavra; cin >> palavra;
+	init();
+	add(palavra);
+	return 0;
+}
