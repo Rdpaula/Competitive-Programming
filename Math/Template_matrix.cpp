82b42373ecc98de4dad04faacd3dba7db2796565
Rdpaula <raphael@72008.hotmail.com>
2020-03-15 21:16:14 -0300

    updating template matrix

--- Math/Template_matrix.cpp
+++ Math/Template_matrix.cpp
@@ -1,107 +1,87 @@
-// A pretty nice and very quick matrix multiplier mod 10^9+7
- 
-#include <iostream>
-#include <cstring>
-#include <cassert>
-#include <random>
-#include <algorithm>
-#include <chrono>
-#include <vector>
-#define ull unsigned long long
-#define ll long long
- 
-const ull MOD = 1000000007L;
- 
-void mult(unsigned int * __restrict__ A, unsigned int * __restrict__ B, unsigned int * __restrict__ C, int Cn, int Am, int Cm)
-{
-    static ull buf[1000];
- 
-    for (int j=0; j<Cm; ++j)
-    {
-        std::fill(buf,buf+Cn,0);
-        int k = 0;
-        for (; k+18<=Am; k+=18)
-        {
-            for (int _k=k; _k<k+18; ++_k)
-                for (int i=0; i<Cn; ++i)
-                    buf[i] += (ull)A[_k*Cn + i]*B[j*Am +_k];
-            for (int i=0; i<Cn; ++i)
-                buf[i] %= MOD;
+const int MOC = 1e9 + 6;
+int MOC2=MOC;
+ll phi(ll n) {
+    ll result = n;
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
         }
-        for (; k<Am; ++k)
-            for (int i=0; i<Cn; ++i)
-                buf[i] += (ull)A[k*Cn + i]*B[j*Am + k];
-        for (int i=0; i<Cn; ++i)
-            C[j*Cn + i] = buf[i]%MOD;
     }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+ll mult(ll a, ll b){
+    return (a*b)%(MOC2);
+}
+
+ll fastpow(ll a,ll b){
+    ll ans=1;
+    for(; b > 0;b/=2){
+        if(b&1) ans = mult(ans,a);
+        a = mult(a,a);
+    }
+    return ans;
+}
+
+template <class T>
+T fexp(T x, long long e) {
+    T ans(1);
+    for(; e > 0; e /= 2) {
+        if(e & 1) ans = ans * x;
+        x = x * x;
+    }
+    // cout<<ans.val<<endl;
+    return ans;
 }
  
- 
-template<typename T>
-struct Matrix {
-    std::vector<T> data;
-    int n;
-    int m;
-    Matrix(int n, int m) : n(n), m(m), data(n*m) {
+template <const int mod = MOC>
+struct modBase {
+    modBase(int v = 0) : val(v) {}
+    int val;
+    void operator += (modBase<mod> o) { *this = *this + o; }
+    void operator -= (modBase<mod> o) { *this = *this - o; }
+    void operator *= (modBase<mod> o) { *this = *this * o; }
+    modBase<mod> operator * (modBase<mod> o) { 
+        if(((long long) val * o.val % mod)<0)return ((long long) val * o.val % mod)+mod;
+        else return ((long long) val * o.val % mod);  
     }
-    T* operator [](int j) {
-        return data.data() + j*n;
+    //modBase<mod> operator / (modBase<mod> o) { return *this * fexp(o, mod - 2); }
+    modBase<mod> operator + (modBase<mod> o) { 
+        if((val + o.val >= mod ? val + o.val - mod : val + o.val)<0) return (val + o.val >= mod ? val + o.val - mod : val + o.val)+mod;
+        else return (val + o.val >= mod ? val + o.val - mod : val + o.val);
     }
-    void fill(T val)
-    {
-        std::fill(data.begin(), data.end(), val);
-    }
-    Matrix operator* (Matrix& B)
-    {
-        assert(this->m==B.n);
-        Matrix<T> C(this->n,B.m);
-        mult((*this)[0],B[0],C[0],C.n,this->m,C.m);
-        return C;
-    }
-    
-    void operator<< (Matrix& B)
-    {
-        assert(this->m==B.n);
-        int Cn = this->n;
-        int Cm = B.m;
-        assert(this->n==Cn);
-        assert(this->m==Cm);
-        auto C = new T[Cn*Cm];
-        mult((*this)[0],B[0],C,Cn,this->m,Cm);
-        std::copy(C,C+Cn*Cm,(*this)[0]);
-        delete[] C;
-    }
-    
-    void operator>> (Matrix& B)
-    {
-        assert(this->m==B.n);
-        int Cn = this->n;
-        int Cm = B.m;
-        assert(B.n==Cn);
-        assert(B.m==Cm);
-        auto C = new T[Cn*Cm];
-        mult((*this)[0],B[0],C,Cn,this->m,Cm);
-        std::copy(C,C+Cn*Cm,B[0]);
-        delete[] C;
-    }
+    modBase<mod> operator - (modBase<mod> o) { return val - o.val < 0 ? val - o.val + mod : val - o.val; }
 };
  
-using Mat = Matrix<unsigned int>;
- 
-template<typename T>
-T power(T &A, T &B, ull m)
-{
-    if (m==0)
-        return B;
-    while (m>1)
-    {
-        if (m&1)
-        {
-            A>>B;
+template<const size_t n, const size_t m, class T = modBase<>>
+struct Matrix {
+    T v[n][m];
+    
+    Matrix(int d = 0) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                v[i][j] = T(0);
+            }
+            if(i < m) {
+                v[i][i] = T(d);
+            }
         }
-        A>>A;
-        m/=2;
     }
-    A>>B;
-    return B;
-}
+    
+    template<size_t mm>
+    Matrix<n, mm, T> operator *(Matrix<m, mm, T> &o) {
+        Matrix<n, mm, T> ans;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < mm; j++) {
+                for(int k = 0; k < m; k++) {
+                    ans.v[i][j] = ans.v[i][j] + v[i][k] * o.v[k][j];
+                }
+            }
+        }
+        return ans;
+    }
+};
