9126c566819608ee35f21bda4528ad2ae9c53c21
Rdpaula <raphael@72008.hotmail.com>
2020-09-16 17:57:54 -0300

    updating double_hash

--- String/Double_hash.cpp
+++ String/Double_hash.cpp
@@ -1,19 +1,28 @@
-pii compute_hash(string const& s) {
+vector<vector<ll> > compute_hash(string const& s) {
     const int p = 31;
     const int m = 1e9 + 9;
     const int p2 = 37;
     const int m2 = 1e9+7;
-    ll hash_value = 0;
+    vector<vector<ll>> hash_value(maxn,vector<ll>(2,0));
+    hash_value[0][0] = 0;
     ll p_pow = 1;
-    ll hash_value2 = 0;
+    hash_value[0][1] = 0;
     ll p_pow2 = 1;
+    
+    ll cont=1;
+    
     for (char c : s) {
-        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        hash_value[cont][0] = (hash_value[cont-1][0] + (c - 'a' + 1) * p_pow) % m;
         p_pow = (p_pow * p) % m;
+        cont++;
     }
+
+    cont=1;
+    
     for (char c : s){
-        hash_value2 = (hash_value2 + (c - 'a' + 1) * p_pow2) % m2;
+        hash_value[cont][1] = (hash_value[cont-1][1] + (c - 'a' + 1) * p_pow2) % m2;
         p_pow2 = (p_pow2 * p2) % m2;
+        cont++;
     }
-    return make_pair(hash_value,hash_value2);
+    return hash_value;
 }
