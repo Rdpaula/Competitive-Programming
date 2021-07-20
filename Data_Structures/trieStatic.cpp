1b86dc6948d9517b1088b48e585c1b34b732916a
Rdpaula <raphael.fonseca@ccc.ufcg.edu.br>
2021-07-20 00:13:09 -0300

    improving trieStatic

--- Data_Structures/trieStatic.cpp
+++ Data_Structures/trieStatic.cpp
@@ -16,10 +16,12 @@ void Add (string s){
 int Find (string s) {
     int idx = 0;
     for (int i = 0; i < sz(s); i++){
-        if (trie[idx][s[i] - 'a'] == 0)
+        if(trie[idx][s[i] - 'a'] == 0){
             return 0;
-        else
+        }
+        else{
             idx = trie[idx][s[i] - 'a'];
+        }
     }
     return finish[idx];
 }
