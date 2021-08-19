d146c4d0042e4790380b878889c2c172596ef8f5
Rdpaula <raphael.fonseca@ccc.ufcg.edu.br>
2021-08-19 12:00:26 -0300

    Smaller size names

--- Math/Mod_Template.cpp
+++ Math/Mod_Template.cpp
@@ -1,19 +1,19 @@
-ll mult(ll a,ll b){
+ll mul(ll a,ll b){
     return a*b%MOD;
 }
-ll fastpow(ll a, ll b){
+ll fpw(ll a, ll b){
     ll calc=1;
     for(;b>=1LL;b/=2LL){
-        if((b&1LL))calc=mult(calc,a);
-        if(b!=1)a=mult(a,a);
+        if((b&1LL))calc=mul(calc,a);
+        if(b!=1)a=mul(a,a);
     }
     return calc;
 }
-ll soma(ll a,ll b){
+ll add(ll a,ll b){
     if(a+b>=MOD)return a+b-MOD;
     else return a+b;
 }
-ll subt(ll a,ll b){
+ll sub(ll a,ll b){
     if(a-b<0)return a-b+MOD;
     else return a-b;
 }
