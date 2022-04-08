b29df6434605265a83d2887a228760b2392571f1
Raphael de Paula Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2022-04-08 01:21:41 -0300

    update tpgeom

--- Geometry/tpgeom.cpp
+++ Geometry/tpgeom.cpp
@@ -1,27 +1,26 @@
-struct pt{
-    ll x;
-    ll y;
+/// sort counter clockwise in (0,0) -> if change the center subtract from coordinates
+struct Point{
+    ll x, y;
+    Point(){}
+    Point(ll x, ll y): x(x), y(y){}
 };
-pt u;
-bool cmp(pt a, pt b) {
-    return a.x < b.x || (a.x == b.x && a.y < b.y);
-}
 
-bool cw(pt a, pt b, pt c) {
-    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) < 0;
-}
-bool ccw(pt a, pt b, pt c) {
-    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) > 0;
-}
-ll operator%(const pt &a, const pt &b){//z-coordinate of cross product
+ll operator%(const Point &a, const Point &b){//z-coordinate of cross product
     return a.x * b.y - a.y * b.x;
 }
-ll operator*(const pt &a, const pt &b){//dot product
+ll operator*(const Point &a, const Point &b){//dot product
     return a.x * b.x + a.y * b.y;
-}
-bool A(const pt &p){
+const Point u(0, 1); /// Point that the line will begin
+bool A(const Point &p){
     return u % p > 0 || (u % p == 0 && u * p > 0);
 }
-bool operator<(const pt &a, const pt &b){
+bool operator<(const Point &a, const Point &b){
     return (A(a) == A(b) && a % b > 0) || (A(a) && !A(b));
 }
+
+// case when points a and b are on the same line from the center, 
+// can be used to solve this edge case in sort
+// check which point is closer to the center
+// int d1 = (a.x - center.x) * (a.x - center.x) + (a.y - center.y) * (a.y - center.y);
+// int d2 = (b.x - center.x) * (b.x - center.x) + (b.y - center.y) * (b.y - center.y);
+// return d1 > d2;
