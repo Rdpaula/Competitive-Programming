1507d01e0bd24c8297ba6d2eda4a1b72c31fbbd4
Raphael Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2023-01-23 23:31:18 -0300

    add initial Flow algorithms

---
+++ Flow/EdmondMaxFlow.cpp
@@ -0,0 +1,48 @@
+/// capacity is adjacency matrix
+/// for undirected graphs u need to creater Enter vertex(2*a) and Exit vertex(2*a + 1), create Enter -> Exit with INF cap
+/// O(V*E^2)
+
+int bfs(int s, int t, vector<int>& parent) {
+    fill(parent.begin(), parent.end(), -1);
+    parent[s] = -2;
+    queue<pair<int, int>> q;
+    q.push({s, INF});
+
+    while (!q.empty()) {
+        int cur = q.front().first;
+        int flow = q.front().second;
+        q.pop();
+
+        for (int next : adj[cur]) {
+            if (parent[next] == -1 && capacity[cur][next]) {
+                parent[next] = cur;
+                int new_flow = min(flow, capacity[cur][next]);
+                if (next == t)
+                    return new_flow;
+                q.push({next, new_flow});
+            }
+        }
+    }
+
+    return 0;
+}
+
+int maxflow(int s, int t) {
+    int flow = 0;
+    vector<int> parent(N);
+    int new_flow;
+
+    while (new_flow = bfs(s, t, parent)) {
+        flow += new_flow;
+        int cur = t;
+        while (cur != s) {
+            int prev = parent[cur];
+            capacity[prev][cur] -= new_flow;
+            capacity[cur][prev] += new_flow;
+            cur = prev;
+        }
+    }
+
+    return flow;
+}
+
