a19a16ae66b1ed7d08c5393847e7967eac00f73c
Raphael Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2023-01-28 12:53:37 -0300

    improve 2D_sum

--- Data_Structures/2D_sum.cpp
+++ Data_Structures/2D_sum.cpp
@@ -1,8 +1,17 @@
-for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-        if(i==0 && j==0)continue;
-        else if(i==0)mat[i][j] += mat[i][j-1];
-        else if(j==0)mat[i][j] += mat[i-1][j];
-        else mat[i][j] += mat[i-1][j] + mat[i][j-1] - mat[i-1][j-1];
+ll mat[maxn][maxn]; // 1 - indexed
+
+void build(){  
+    for(int i=1;i<maxn;i++){
+        for(int j=1;j<maxn;j++){
+            if(i==1 && j==1)continue;
+            else if(i==1)mat[i][j] += mat[i][j-1];
+            else if(j==1)mat[i][j] += mat[i-1][j];
+            else mat[i][j] += mat[i-1][j] + mat[i][j-1] - mat[i-1][j-1];
+        }
     }
 }
+
+ll query(ll a,ll b, ll c, ll d){
+    return mat[c][d] - (b > 1) * mat[c][b-1] - (a > 1) * (mat[a-1][d]) + (a > 1 && b > 1) * mat[a-1][b-1];
+}
+
