4e4576655f300e2cd6147402afb4b2b2be527813
Raphael de Paula Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2023-01-28 11:17:47 -0300

    Put advices in Iterative Segment Tree

--- Data_Structures/SegtIterative.cpp
+++ Data_Structures/SegtIterative.cpp
@@ -1,5 +1,5 @@
-ll n;
-ll tree[2*maxn]; // vet is 0-indexed
+ll n; // vet is 0-indexed, set MAXN to power of 2(2^18 for 2*10^5 constant) and fill tree with neutral values
+ll tree[2*maxn]; 
  
 ll query(ll l, ll r){
     l += n, r += n;
