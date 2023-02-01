cb2c77c9a9e46f2a0a68ea4b5dbfd10bf57014f5
Raphael de Paula Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2023-01-31 21:03:21 -0300

    Improve Lazy Segt

--- Data_Structures/Lazy_Segtree.cpp
+++ Data_Structures/Lazy_Segtree.cpp
@@ -1,14 +1,12 @@
-int vet[maxn],segt[4*maxn],lazy[4*maxn];
+ll vet[maxn],segt[4*maxn],lazy[4*maxn];
+
+#define NEUTRAL -1
+
 void add(int id,int l,int r,int val){
     // cout<<l<<" "<<r<<" "<<val<<endl;
-    if((l==r && l==val) || val==0){ 
- 
-    }
-    else{
-        segt[id]=val,lazy[id]=val;
-    }
+    segt[id] = val,lazy[id] = val;
 }
-void update(int id,int l,int r,int x,int y,int val){
+void update(ll id,int l,ll r,ll x,ll y,ll val){
     if(x<=l && r<=y)add(id,l,r,val);
     else if(l>y || r<x)return;
     else{
@@ -21,9 +19,9 @@ void update(int id,int l,int r,int x,int y,int val){
         segt[id]=max(segt[id<<1],segt[id<<1|1]);
     }
 }
-int query(int id,int l,int r,int x,int y){
+int query(ll id,ll l,ll r,ll x,ll y){
     if(x<=l && r<=y)return segt[id];
-    else if(l>y || r<x)return -1;
+    else if(l>y || r<x)return NEUTRAL;
     else{
         int mid=(l+r)>>1;
         add(id<<1,l,mid,lazy[id]);
