c0a24a758edb55544131c88ed2dd8928b76afa33
Raphael Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2023-02-06 22:32:02 -0300

    add Mo Algorithm

---
+++ Data_Structures/Mo_Algorithm.cpp
@@ -0,0 +1,54 @@
+#define INF 1e18+9
+#define endl '\n'
+
+void remove(idx);  // TODO: remove value at idx from data structure
+void add(idx);     // TODO: add value at idx from data structure
+int get_answer();  // TODO: extract the current answer of the data structure
+
+int block_size;
+
+ll cnt[maxn];
+ll ans = 0;
+
+vector<ll> vet;
+
+struct Query {
+    int l, r, idx;
+    bool operator<(Query other) const
+    {
+        return make_pair(l / block_size, r) <
+               make_pair(other.l / block_size, other.r);
+    }
+};
+
+vector<ll> mo_s_algorithm(vector<Query> queries) {
+    vector<ll> answers(queries.size());
+    sort(queries.begin(), queries.end());
+
+    // TODO: initialize data structure
+    
+    ll cur_l = 0;
+    ll cur_r = -1;
+    // invariant: data structure will always reflect the range [cur_l, cur_r]
+    for (Query q : queries) {
+        while (cur_l > q.l) {
+            cur_l--;
+            add(cur_l);
+        }
+        while (cur_r < q.r) {
+            cur_r++;
+            add(cur_r);
+        }
+        while (cur_l < q.l) {
+            remove(cur_l);
+            cur_l++;
+        }
+        while (cur_r > q.r) {
+            remove(cur_r);
+            cur_r--;
+        }
+        answers[q.idx] = get_answer();
+    }
+    return answers;
+}
+
