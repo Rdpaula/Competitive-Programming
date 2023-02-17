61d7b07e0d0bc2bdd18c7a45892b2c2f4ecd8103
Raphael de Paula Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2023-02-17 08:04:24 -0300

    Add template

---
+++ template.cpp
@@ -0,0 +1,47 @@
+#include "bits/stdc++.h"
+#define ll long long
+#define pii pair<ll,ll>
+#define pi acos(-1)
+#define maxn 200009 /// I want to stay forever with you little star
+#define MOD 1000000007
+#define F first
+#define S second
+#define rep(i, a, b) for(ll i = a; i < (b); ++i)
+#define per(i, b, a) for(ll i = b-1; i>=a ; i--)
+#define sz(x) (ll)((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sor(x) sort(all(x)) 
+#define getunique(v) {sort(all(v)); v.erase(unique(all(v)), v.end());}
+#define rsz resize
+#define ins insert 
+#define ft front()
+#define bk back()
+#define pb push_back
+#define eb emplace_back 
+#define pf push_front
+using namespace std;
+ 
+#ifdef LOCAL
+#include "debug.h"
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+ 
+#define INF 1e18+9
+#define endl '\n'
+
+void solvetask(){
+    
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+//    cin>>t;
+    while(t--) solvetask();
+    return 0;
+}
