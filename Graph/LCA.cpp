476b7ac1301becfb92b1750d54b61da57b06489b
Raphael de Paula Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2023-02-23 22:27:17 -0300

    Change to template code

--- Graph/LCA.cpp
+++ Graph/LCA.cpp
@@ -1,15 +1,16 @@
-int timer,l;
-vector<int> tin, tout;
-vector<vector<int>> up;
-void calc(int v, int p){
+ll timer,l;
+vector<ll> tin, tout;
+vector<vector<ll>> up;
+vector<ll> adj[maxn];
+
+void calc(ll v, ll p){
     tin[v] = ++timer;
     up[v][0] = p;
-    for (int i = 1; i <= l; ++i)
+    for (ll i = 1; i <= l; ++i)
         up[v][i] = up[up[v][i-1]][i-1];
 
-    for (int u : adj2[v]) {
+    for (ll u : adj[v]) {
         if (u != p){
-            res[u]+=res[v];
             calc(u, v);
         }
     }
@@ -17,28 +18,28 @@ void calc(int v, int p){
     tout[v] = ++timer;
 }
 
-bool is_ancestor(int u, int v)
+bool is_ancestor(ll u, ll v)
 {
     return tin[u] <= tin[v] && tout[u] >= tout[v];
 }
-int lca(int u, int v)
+ll lca(ll u, ll v)
 {
     if (is_ancestor(u, v))
         return u;
     if (is_ancestor(v, u))
         return v;
-    for (int i = l; i >= 0; --i) {
+    for (ll i = l; i >= 0; --i) {
         if (!is_ancestor(up[u][i], v))
             u = up[u][i];
     }
     return up[u][0];
 }
 
-void preprocess(int root) {
+void preprocess(ll root) {
     tin.resize(n+1);
     tout.resize(n+1);
     timer = 0;
     l = ceil(log2(n+1));
-    up.assign(n+1, vector<int>(l + 1));
+    up.assign(n+1, vector<ll>(l + 1));
     calc(root, root);
 }
