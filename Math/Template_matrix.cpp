472ad7570d3f1fedddadccc3a9d70714fd1e0f7a
Raphael de Paula Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2023-04-12 22:52:33 -0300

    Add fpw and sub operations

--- Math/Template_matrix.cpp
+++ Math/Template_matrix.cpp
@@ -8,10 +8,21 @@ using namespace std;
 ll mul(ll a,ll b){
     return a*b%MOD;
 }
-
+ll fpw(ll a, ll b){
+    ll calc=1;
+    for(;b>=1LL;b/=2LL){
+        if((b&1LL))calc=mul(calc,a);
+        if(b!=1)a=mul(a,a);
+    }
+    return calc;
+}
 ll add(ll a,ll b){
     if(a+b>=MOD)return a+b-MOD;
     else return a+b;
+}
+ll sub(ll a,ll b){
+    if(a-b<0)return a-b+MOD;
+    else return a-b;
 }
 
 struct Matrix {
