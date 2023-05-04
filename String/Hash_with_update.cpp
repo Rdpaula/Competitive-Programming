94601de3798c233ba135b0a5d162d5b2bd2a5bc1
Raphael de Paula Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2023-05-04 01:24:45 -0300

    Improve Hash_with_update

--- String/Hash_with_update.cpp
+++ String/Hash_with_update.cpp
@@ -1,16 +1,11 @@
-///Thanks AmandioF
+//Thanks AmandioF
 
-#include <bits/stdc++.h>
 #define MAX 100007
 
-using namespace std;
-
-typedef long long ll;
-
-ll h[4*MAX], power[MAX], inv[MAX];
+ll h[MAX], power[MAX], inv[MAX];
 ll base[2] = {317, 307};
 ll mod[2] = {104000717, 104000711};
-int n;
+ll n;
 
 ll gcd(ll a, ll m){
     ll x = 1, y = 0, ini = m;
@@ -34,7 +29,6 @@ ll gcd(ll a, ll m){
 void init(){
     power[0] = base[0];
     inv[0] = gcd(base[0], mod[0]);
-    cout << inv[0] << endl;
     for(int i = 1; i < MAX; i++){
         power[i] = (power[i-1]*base[0])%mod[0];
         inv[i] = (inv[i-1]*inv[0])%mod[0];
@@ -42,7 +36,6 @@ void init(){
 }
 
 void update(int idx, char now, char old = '$'){
-    cout << now << " " << old << endl;
     int pos = idx;
     if(old == '$') for(; idx <= n; idx += idx&(-idx)) h[idx] = (h[idx] + (now-'a'+1)*power[pos])%mod[0];
     else{
@@ -64,26 +57,7 @@ ll query(int idx){
     return hash;
 }
 
-ll query(int l, int r){ /// 1-indexed
+ll query(int l, int r){ // 1-indexed
     return (((query(r)-query(l-1)+ mod[0])%mod[0])*inv[l])%mod[0];
 }
-int main(){
-    string txt; cin >> txt;
-    n = txt.size();
-    init();
-    getHash(txt);
-    int k; cin >> k;
-    while(k--){
-        int tipo; cin >> tipo;
-        if(!tipo){
-            int l, r; cin >> l >> r;
-            cout << query(l, r) << endl;
-        }else{
-            int idx;
-            char letra; cin >> idx >> letra;
-            update(idx, letra, txt[idx-1]);
-            txt[idx-1] = letra;
-        }
-    }
-    return 0;
-}
+// init -> getHash(s)
